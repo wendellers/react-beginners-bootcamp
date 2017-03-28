6e1584cedd93aef723e01ab125756c40859f78c5
Adam Goldman <adamgoldman7@gmail.com>
2017-03-29 01:25:52 +0800

    init

---
+++ webpack.config.babel.js
@@ -0,0 +1,49 @@
+import path from 'path'
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const ENV = process.env.NODE_ENV || 'production'
+const isProd = ENV === 'production'
+
+export default {
+  entry: [
+    'react-hot-loader/patch',
+    './src/client',
+  ],
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader', include: /src/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(woff(2)|ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  devtool: isProd ? false : 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    port: 8000,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'client', 'index.html'),
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+}
