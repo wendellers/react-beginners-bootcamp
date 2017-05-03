1c1d99373d33b397e2b03461120f535b2c02377e
goldylucks <adamgoldman7@gmail.com>
2017-05-03 14:48:35 +0800

    module-1 step-12

--- src/client/Auth.jsx
+++ src/client/Auth.jsx
@@ -1,45 +1,52 @@
 import React from 'react'
 
-const AuthApp = () =>
-  <div className="container app-container">
-    <h2>Auth app!</h2>
-    <div className="row">
-      <div className="col-md-4">
-        <h3>Controls</h3>
+class AuthApp extends React.Component {
+  state = {
+    nameInput: '',
+    emailInput: '',
+    passwordInput: '',
+    name: '',
+    role: 'guest',
+  }
+
+  render() {
+    return (
+      <div className="container app-container">
+        <h2>Auth app!</h2>
         <div className="row">
-          <div className="col-md-12 form-group">
-            <input placeholder="Name" className="form-control" />
+          <div className="col-md-4">
+            <h3>Controls</h3>
+            <div className="row">
+              <div className="col-md-12 form-group">
+                <input placeholder="Name" value={this.state.nameInput} className="form-control" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12 form-group">
+                <input type="email" value={this.state.emailInput} placeholder="Email" className="form-control" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12 form-group">
+                <input type="password" value={this.state.passwordInput} placeholder="Password" className="form-control" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <button className="btn btn-primary btn-block">Signup</button>
+              </div>
+              <div className="col-md-6">
+                <button className="btn btn-default btn-block">Login</button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 form-group">
-            <input type="email" placeholder="Email" className="form-control" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 form-group">
-            <input type="password" placeholder="Password" className="form-control" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6">
-            <button className="btn btn-primary btn-block">Signup</button>
-          </div>
-          <div className="col-md-6">
-            <button className="btn btn-default btn-block">Login</button>
+          <div className="col-md-2 col-md-offset-2">
+            <h3>Output</h3>
+            <div className="well" />
           </div>
         </div>
       </div>
-      <div className="col-md-2 col-md-offset-2">
-        <h3>Output</h3>
-        <div className="well">
-          <p>Hello guest</p>
-          <p>Welcome back Spidy</p>
-          <p>Welcome aboard Spidy!</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
+    )
+  }
+}
 export default AuthApp
--- src/client/Guessnumber.jsx
+++ src/client/Guessnumber.jsx
@@ -9,30 +9,38 @@ const styles = {
   },
 }
 
-const GuessnumberApp = () =>
-  <div className="container app-container">
-    <h2>Guess Number</h2>
-    <p className="lead">Guess a number between 1-100</p>
-    <div className="row">
-      <div className="col-md-4">
-        <h3>Controls</h3>
-        <div className="input-group">
-          <input className="form-control" type="number" placeholder="Place your guess here" min={1} max={100} />
-          <span className="input-group-btn">
-            <button className="btn btn-primary">Guess</button>
-          </span>
-        </div>
-      </div>
-      <div className="col-md-2 col-md-offset-2">
-        <h3>Output</h3>
-        <div className="well">
-          <div style={styles.failure}>Too low ...</div>
-          <div style={styles.failure}>Too high ...</div>
-          <div style={styles.success}>Success! The number is 73</div>
-        </div>
-      </div>
-    </div>
-  </div>
+class GuessnumberApp extends React.Component {
+  state = {
+    guessInput: '',
+    guessed: null,
+    number: 73,
+  }
 
+  render() {
+    return (
+      <div className="container app-container">
+        <h2>Guess Number</h2>
+        <p className="lead">Guess a number between 1-100</p>
+        <div className="row">
+          <div className="col-md-4">
+            <h3>Controls</h3>
+            <div className="input-group">
+              <input className="form-control" type="number" value={this.state.guessInput} placeholder="Place your guess here" min={1} max={100} />
+              <span className="input-group-btn">
+                <button className="btn btn-primary">Guess</button>
+              </span>
+            </div>
+          </div>
+          <div className="col-md-2 col-md-offset-2">
+            <h3>Output</h3>
+            <div className="well">
+              <div style={styles.success}>Success! The number is {this.state.number}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default GuessnumberApp
